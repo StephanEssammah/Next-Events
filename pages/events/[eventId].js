4cd790a2d691d2d844e0b230d792576b34be8d88
unknown <stephan.essammah@hotmail.com>
2022-03-16 10:26:15 +0100

    added logic to eventId page to use dynamic data instead of static

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,4 +1,6 @@
-export default function Event() {
+import { events } from "./index";
+
+export default function Event({ event }) {
   return (
     <div className="flex flex-col p-4 h-screen bg-gray-800 text-white">
       <div className="py-2 flex justify-between bg-gray-700">
@@ -7,10 +9,19 @@ export default function Event() {
       </div>
       <div className="w-full h-1/4 bg-gray-400" />
       <div className="mb-4">
-        <h1 className="text-3xl">Event Title</h1>
-        <p>Location</p>
+        <h1 className="text-3xl">{event.title}</h1>
+        <p>{event.location}</p>
       </div>
-      <p className="text-gray-400">Event description</p>
+      <p className="text-gray-400">{event.description}</p>
     </div>
   );
 }
+
+export const getServerSideProps = async (context) => {
+  const eventId = context.params.eventId;
+  const event = events.find((event) => event.id === Number(eventId));
+
+  return {
+    props: { event },
+  };
+};
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,19 +1,28 @@
-export default function Events() {
-  const events = [
-    {
-      title: "Kickoff Event",
-      location: "Oslo",
-    },
-    {
-      title: "Social Event",
-      location: "Oslo",
-    },
-    {
-      title: "Another Event",
-      location: "Oslo",
-    },
-  ];
+import { useRouter } from "next/router";
 
+export const events = [
+  {
+    title: "Kickoff Event",
+    location: "Oslo",
+    description: "Lorum ipsum kickoff event",
+    id: 1,
+  },
+  {
+    title: "Social Event",
+    location: "Oslo",
+    description: "Lorum ipsum social event",
+    id: 2,
+  },
+  {
+    title: "Another Event",
+    location: "Oslo",
+    description: "Lorum ipsum another event",
+    id: 3,
+  },
+];
+
+export default function Events() {
+  const router = useRouter();
   return (
     <div className="flex flex-col p-4 h-screen bg-gray-800 text-white">
       {events.map((event, index) => {
@@ -21,10 +30,11 @@ export default function Events() {
           <div
             key={index}
             className="flex justify-between bg-gray-600 p-4 mb-4"
+            onClick={() => router.push(`/events/${event.id}`)}
           >
             <div>
-              <h1>Kickoff Event</h1>
-              <p>Location</p>
+              <h1>{event.title}</h1>
+              <p>{event.location}</p>
             </div>
             <div>
               <p>lo</p>
