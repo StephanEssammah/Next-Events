c5e8b00ed51a5d55e2a6540775d52b704cab55a3
unknown <stephan.essammah@hotmail.com>
2022-03-31 11:27:53 +0200

    Responsiveness and general styling of profile page

--- pages/profile.js
+++ pages/profile.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { signOut } from "next-auth/react";
 import { getSession } from "next-auth/react";
+import { FaEdit } from "react-icons/fa";
 
 const Profile = () => {
   const handleSignout = async () => {
@@ -8,23 +9,28 @@ const Profile = () => {
   };
 
   return (
-    <div className="flex flex-col p-4 h-full bg-gray-800 text-white justify-between lg:px-20">
-      <h1 className="mb-2 text-5xl font-semibold">Profile</h1>
-      <div>
-        <div className="mb-4">
-          <p className="">Email:</p>
-          <p className="text-white text-opacity-50">
-            stephan.essammah@appliedtechnology.se
-          </p>
+    <div className="flex flex-col p-4 h-full items-center bg-gray-800 text-white justify-between lg:px-20 sm:justify-center">
+      <div className="w-full max-w-md sm:p-4 sm:bg-white sm:bg-opacity-10 sm:mb-8 rounded">
+        <div className="flex justify-between items-center mb-8">
+          <h1 className="mb-2 text-5xl font-semibold">Profile</h1>
+          <FaEdit size="2em" />
         </div>
-        <div className="mb-4">
-          <p className="">Display Name:</p>
-          <p className="text-white text-opacity-50">Stephan Essammah</p>
+        <div>
+          <div className="mb-4">
+            <p className="">Email:</p>
+            <p className="text-white text-opacity-50">
+              stephan.essammah@appliedtechnology.se
+            </p>
+          </div>
+          <div className="mb-4">
+            <p className="">Display Name:</p>
+            <p className="text-white text-opacity-50">Stephan Essammah</p>
+          </div>
         </div>
       </div>
       <button
         onClick={handleSignout}
-        className="bg-red-400 p-4 rounded font-medium w-64"
+        className="bg-red-400 p-4 rounded font-medium w-full max-w-md"
       >
         Sign out
       </button>
